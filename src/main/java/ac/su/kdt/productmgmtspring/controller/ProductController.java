3de8023acc201a3a52cc66393455475ec217fb35
wild-mental <wildmental@knou.ac.kr>
2025-05-02 11:06:02 +0900

    5/2 Controller URL 매핑 및 Service 플레이스홀딩

--- src/main/java/ac/su/kdt/productmgmtspring/controller/ProductController.java
+++ src/main/java/ac/su/kdt/productmgmtspring/controller/ProductController.java
@@ -4,17 +4,18 @@ import ac.su.kdt.productmgmtspring.domain.dto.ProductRequestDTO;
 import ac.su.kdt.productmgmtspring.domain.dto.ProductResponseDTO;
 import ac.su.kdt.productmgmtspring.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/products")
 public class ProductController {
     private final ProductService productService;
-    // 상품 등록
+    // CREATE 상품 등록
     @PostMapping
     public ProductResponseDTO create(
         @RequestBody ProductRequestDTO productRequestDTO
@@ -22,4 +23,63 @@ public class ProductController {
         // 상품 등록 로직
         return productService.create(productRequestDTO);
     }
+    // READ 상품 조회
+    // 1) 상품 전체 조회 (전체 리스트)
+    @GetMapping("/all")
+    public List<ProductResponseDTO> readAll() {
+        // 상품 전체 조회 로직
+        return productService.readAll();
+    }
+    // 2) 상품 전체 조회 (페이징 처리)
+    @GetMapping
+    public Page<ProductResponseDTO> readPage() {
+        // 상품 전체 조회 로직
+        return productService.readPage();
+    }
+
+    // 3) 상품 단건 조회
+    @GetMapping("/{id}")
+    public ProductResponseDTO readOne(
+        @PathVariable Long id
+    ) {
+        // 상품 단건 조회 로직
+        return productService.readOne(id);
+    }
+
+    // UPDATE 상품 수정
+    @PutMapping("/{id}")
+    public ProductResponseDTO put(
+        @PathVariable Long id,
+        @RequestBody ProductRequestDTO productRequestDTO
+    ) {
+        // 상품 수정 로직
+        return productService.put(id, productRequestDTO);
+    }
+    // PATCH 상품 수정
+    @PatchMapping("/{id}")
+    public ProductResponseDTO patch(
+        @PathVariable Long id,
+        // 데이터 수정 포인트가 줄어들지만, Request 에서 수신한 필드별 검사가 필요함
+        @RequestBody ProductRequestDTO productRequestDTO
+    ) {
+        // 상품 수정 로직
+        return productService.patch(id, productRequestDTO);
+    }
+
+    // DELETE 상품 삭제
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> delete(
+        @PathVariable Long id
+    ) {
+        // 상품 삭제 실패 케이스에 대한 상세 핸들링 및 응답 메시지 처리
+        if (productService.delete(id)) {
+            return ResponseEntity.ok()
+                .body("상품 삭제에 성공했습니다.");
+        } else {
+            // TODO : 예외 처리
+            return ResponseEntity.badRequest()
+                .body("상품 삭제에 실패했습니다.");
+        }
+    }
+
 }
--- src/main/java/ac/su/kdt/productmgmtspring/service/ProductService.java
+++ src/main/java/ac/su/kdt/productmgmtspring/service/ProductService.java
@@ -6,9 +6,11 @@ import ac.su.kdt.productmgmtspring.domain.dto.ProductResponseDTO;
 import ac.su.kdt.productmgmtspring.domain.entity.Product;
 import ac.su.kdt.productmgmtspring.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,5 +25,29 @@ public class ProductService {
         product.setStatus(productRequestDTO.getStatus());
         Product savedProduct = productRepository.save(product);
         return ProductResponseDTO.fromEntity(savedProduct);
+    }
+
+    public List<ProductResponseDTO> readAll() {
+        return null;
+    }
+
+    public Page<ProductResponseDTO> readPage() {
+        return null;
+    }
+
+    public ProductResponseDTO readOne(Long id) {
+        return null;
+    }
+
+    public ProductResponseDTO put(Long id, ProductRequestDTO productRequestDTO) {
+        return null;
+    }
+
+    public ProductResponseDTO patch(Long id, ProductRequestDTO productRequestDTO) {
+        return null;
+    }
+
+    public boolean delete(Long id) {
+        return false;
     }
 }
